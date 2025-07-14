51426d443b3bc29124f2e75a2c7795adf0e5a372
Jinseok Kim <jinseok.kim970@gmail.com>
2025-07-14 20:32:36 +0900

    Fix apriltag_ros param(typo)

--- jetcobot_bringup/launch/bringup.launch.py
+++ jetcobot_bringup/launch/bringup.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
         name="apriltag_node",
         output="screen",
         parameters=[PathJoinSubstitution([
-                FindPackageShare('apriltag_ros'), 'config', 'tags_36h11.yaml'])
+                FindPackageShare('apriltag_ros'), 'cfg', 'tags_36h11.yaml'])
         ],
         remappings=[
             ('camera_info', '/camera_info'),
